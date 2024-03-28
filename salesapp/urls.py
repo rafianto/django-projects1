c7a0c18e3e06369d95fdb1a24df35f6c52480aa1
Mulyono Rafianto - Portofolio <mulyonorafianto@gmail.com>
2024-03-28 10:16:06 +0700

    ok tambahan keeteranga

--- salesapp/urls.py
+++ salesapp/urls.py
@@ -6,7 +6,7 @@ from . import views
 from blog import views as blogViews
 from about import views as aboutViews
 
-# tambahan url di tempatkan di bawah ini siap
+# tambahan url di index main
 urlpatterns = [
     path('admin/', admin.site.urls,name='administrator'),
     path('',views.index,name='index-satu'),

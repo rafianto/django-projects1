3c649906cc9232ae4cd123d214ee432670ac8959
Mulyono Rafianto - Portofolio <mulyonorafianto@gmail.com>
2024-03-28 09:41:23 +0700

    Peruhan 20240328

--- salesapp/urls.py
+++ salesapp/urls.py
@@ -6,7 +6,7 @@ from . import views
 from blog import views as blogViews
 from about import views as aboutViews
 
-# tambahan url di tempatkan di bawah ini
+# tambahan url di tempatkan di bawah ini siap
 urlpatterns = [
     path('admin/', admin.site.urls,name='administrator'),
     path('',views.index,name='index-satu'),
